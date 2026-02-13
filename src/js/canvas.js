3a725fd2219a8b95b878255f35ecadb3e8d87b43
James Bamber <james.bamber.05@gmail.com>
2026-02-13 12:24:23 +0100

    better ui

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,10 +1,13 @@
 function draw_point(pt, ctx) {
+    ctx.fillStyle = "rgb(217, 217, 217)";
+
     ctx.beginPath();
     ctx.arc(pt.x, pt.y, 5, 0, Math.PI * 2, true);
     ctx.fill();
 }
 
 function draw_line(pt1, pt2, ctx) {
+    ctx.strokeStyle = "rgb(217, 217, 217)";
     ctx.lineWidth = 2;
 
     ctx.beginPath();
